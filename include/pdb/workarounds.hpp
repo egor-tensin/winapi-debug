5759798155584cb95fe133c53bf51a3ddd30e4fb
Egor Tensin <Egor.Tensin@gmail.com>
2021-05-16 19:17:21 +0300

    remove unused workarounds.hpp

--- include/pdb/workarounds.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2020 Egor Tensin <Egor.Tensin@gmail.com>
-// This file is part of the "winapi-debug" project.
-// For details, see https://github.com/egor-tensin/winapi-debug.
-// Distributed under the MIT License.
-
-#pragma once
-
-#define PDB_UNUSED_PARAMETER(...) (void)(__VA_ARGS__)
